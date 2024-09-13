1e6580de7ab671968ae7c6b77e3e6e4f13887411
sumit <sk7627577@gmail.com>
2024-09-13 20:27:23 +0530

    update

--- src/app/(home)/component/CommonSection.tsx
+++ src/app/(home)/component/CommonSection.tsx
@@ -24,21 +24,18 @@ const CommonSection: React.FC<Props> = ({ data }) => {
         <SectionHeading {...data} />
         <div className="grid lg:grid-cols-3 grid-cols-1 gap-6 mt-8">
           {data.data.map((item, index) => (
-            <div
-              className="shadow-2xl group group-hover:scale-105 duration-500"
-              key={index}
-            >
-              <div className="relative w-full aspect-square">
+            <div className="shadow-2xl group  duration-500" key={index}>
+              <div className="relative w-full aspect-square overflow-hidden">
                 <Image
                   src={item.img}
                   alt={item.label}
                   fill
-                  className="object-cover"
+                  className="object-cover group-hover:scale-150 duration-1000 transition ease-in-out"
                 />
-                <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+                <div className="absolute bottom-0 left-0 w-full h-[25vh] duration-500 ease-in-out group-hover:h-full group-[&:hover]:bg-blue-primary/70 flex justify-center items-center">
                   <Link
                     href={item.href}
-                    className="text-white uppercase border px-4 py-2 font-p-d text-lg bg-orange-primary hover:bg-orange-secondary duration-500 rounded-sm"
+                    className="text-white uppercase border px-4 py-2 font-p-d text-lg bg-transparent hover:bg-orange-secondary duration-500 rounded-sm"
                   >
                     {item.label}
                   </Link>
