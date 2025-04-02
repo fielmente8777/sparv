1235fa2d131854285165bbff30aff408ede96e32
sumit <sk7627577@gmail.com>
2025-04-02 10:04:23 +0530

    update configuration and adjust WhatsApp button position

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    /* config options here */
   output: "export",
   images: {
     unoptimized: true,
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
+    //   },
+    // ],
+    // dangerouslyAllowSVG: true,
   },
-  reactStrictMode: true,
-  swcMinify: true,
   trailingSlash: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
