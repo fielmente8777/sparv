f99a5d3c2cbce0a704f2c8f7d04fd46854fa73bf
Hem Bhadur <sushilkc2611@gmail.com>
2025-04-29 14:09:14 +0530

    implement chat bot with implemnting country code and input date

---
+++ src/components/chatbot/RenderChatBot.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React from "react";
+import ChatbotWidget from "./ChatbotWidget";
+import axios from "axios";
+
+export interface FormData {
+  name: string;
+  email: string;
+  phone: string;
+  [key: string]: string; // For additional dynamic fields
+}
+
+const RenderChatBot = () => {
+  const welcomeMessage =
+    " Welcome to Sparv, Thank you for reaching out to Goa's most preferred Hospitality.";
+  const thankYouMessage = `Thank you so much for filling up the details, one of our representative will speak to you as soon as possible!`;
+
+  const messageFlows = [
+    { key: "name", question: "What's your Name?", type: "text" },
+    { key: "email", question: "What's your Email ?", type: "email" },
+    { key: "phone", question: "What's your Phone Number?", type: "number" },
+    { key: "check-in", question: "Check-in date!", type: "date" },
+    { key: "check-out", question: "Check-out date!", type: "date" },
+    { key: "number of guest", question: "Number of guest!", type: "text" },
+  ];
+
+  const handleSumbit = async (formData: Record<string, string | string[]>) => {
+    // Extract the known fields
+    const { name, email, phone, ...otherFields } = formData;
+    const description = Object.entries(otherFields)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join(",");
+
+    try {
+      // const { data } = await axios.post(
+      //   "https://nexon.eazotel.com/eazotel/addcontacts",
+      //   {
+      //     Domain: "sparvhospitality",
+      //     Contact: `${phone}`,
+      //     email: `${email}`,
+      //     Description: description,
+      //     Name: `${name}`,
+      //     Remark: "",
+      //     Subject: null,
+      //     created_from: "Chatbot",
+      //   },
+      //   {
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   }
+      // );
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <ChatbotWidget
+        onSubmit={handleSumbit}
+        messages={welcomeMessage}
+        finalMessage={thankYouMessage}
+        messageFlows={messageFlows}
+        title="Sparv"
+        theme={"#00486C"}
+        openInterval={2000}
+        logo={"/images/log2.png"}
+      />
+    </div>
+  );
+};
+
+export default RenderChatBot;
