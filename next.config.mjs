b2397248a96e809e0301800b0dc399bf48d75565
sumit <sk7627577@gmail.com>
2025-07-22 12:53:05 +0530

    fix: add missing license field for react-razorpay package

--- next.config.mjs
+++ next.config.mjs
@@ -53,6 +53,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "cdn.builder.io",
       },
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+      },
     ],
     dangerouslyAllowSVG: true,
   },
