133ea84365695d9b559bf387ddb1a59cdb669293
fielmente8777 <122347218+fielmente8777@users.noreply.github.com>
2026-06-10 17:24:58 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,6 @@ const nextConfig = {
   reactStrictMode: true,
   
   images: {
-    unoptimized: true,
     formats: ["image/avif", "image/webp"],
 
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
